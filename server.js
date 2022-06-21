895a0856e7d895d441cc64e31c350f8f5ed7a424
Billal-Hossen <mddipu867@gmail.com>
2022-06-21 18:34:11 +0600

    check

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// const express= require("express");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 8080;
-
-// app.use('/',(req,res)=>{
-//   res.send("Surver running");
-// })
-// app.listen(port,()=>{
-//   console.log(`listening from port ${port}`);
-// })
-//NmBCRemvQs4auiaO
 
 require('dotenv/config');
 const app=require('./app');
@@ -22,5 +10,5 @@ mongoose.connect(DB)
 .catch(err=>console.log("MongoDB Connecton Failed!!"));
  const port=process.env.PORT || 3005;
 
- 
+
 app.listen(port, ()=>console.log(`Listing on port ${port}.....`));
